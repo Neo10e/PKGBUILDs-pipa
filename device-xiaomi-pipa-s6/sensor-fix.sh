318fa211bdb814bc65c3c378faf504b2162f0c08
Neo10 <151381260+Neo10e@users.noreply.github.com>
2026-05-24 18:33:46 +0200

    device-xiaomi-pipa-s6: migration to new s6-frontend

--- device-xiaomi-pipa-s6/sensor-fix.sh
+++ device-xiaomi-pipa-s6/sensor-fix.sh
@@ -3,9 +3,9 @@ case "$1" in
   post)
     (
       sleep 2
-      /usr/bin/s6-svc -r /run/service/iio-sensor-proxy-libssc
-      /usr/bin/s6-svc -r /run/service/hexagonrpcd-sdsp
-      /usr/bin/s6-svc -r /run/service/iio-sensor-proxy-libssc
+      /usr/bin/s6 process restart iio-sensor-proxy-libssc
+      /usr/bin/s6 process restart hexagonrpcd-sdsp
+      /usr/bin/s6 process restart iio-sensor-proxy-libssc
     ) >/dev/null 2>&1 &
   ;;
 esac
